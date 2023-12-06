0d4c14f5c03b08b1ca0dd182eda92f00a74bf185
kitKimBeomSeok <126782965+kitKimBeomSeok@users.noreply.github.com>
2023-12-07 02:45:59 +0900

    faker에서 리뷰 생성을 제외하여 생성후 리뷰 삽입 시도 성공 db로 결과 확인 완료 추후에 사용자 입력을 받아서 하도록 구현

---
+++ routers/reviewController.js
@@ -0,0 +1,55 @@
+const { Router } = require("express");
+const accommodationRouter = require('./accommodationController');
+const { Review } = require("../models/review");
+const { Reservation } = require("../models/reservation");
+const { isValidObjectId } = require("mongoose");
+const { Accommodation } = require("../models/accommodation");
+const { Member } = require("../models/member");
+
+const reviewRouter = Router();
+reviewRouter.post("/", async (req, res) => {
+    try {
+
+        const { star, reviewContent, reserveId} = req.body;
+        const reservation = await Reservation.findById(reserveId)
+
+        // 여기서 DateInfoSchema의 endDate와 현재 날짜를 비교하여 리뷰 작성 가능 여부 확인
+        const currentDate = new Date();
+
+        const isReviewAllowed = reservation.dateInfo[0].endDate.getTime() < currentDate.getTime();
+        if (!isReviewAllowed)
+            return res.status(400).send({ error: "Review can only be written after the end date" });
+
+        const review = new Review({ star, review : reviewContent});
+        reservation.review = review;
+
+        await review.save();
+        await reservation.save();
+        return res.send({ review });
+
+    } catch (err) {
+        console.log(err);
+        return res.status(400).send({ error: err.message });
+    }
+});
+
+// reviewRouter.get("/", async (req, res) => {
+//     const { accommodationId } = req.params;
+//     if (!isValidObjectId(accommodationId))
+//         return res.status(400).send({ error: "accommodationId is invalid" });
+//     const review = await Comment.find({ accommodation: accommodationId });
+//     return res.send({ review });
+// });
+//
+// reviewRouter.get("/:accommodationId/:memberId", async (req, res) => {
+//     const { accommodationId, memberId } = req.params;
+//
+//     if (!isValidObjectId(accommodationId) || !isValidObjectId(memberId))
+//         return res.status(400).send({ error: "유효하지 않은 accommodationId 또는 memberId" });
+//
+//     const reviews = await Comment.find({ accommodation: accommodationId, member: memberId });
+//     return res.send({ reviews });
+// });
+
+
+module.exports = reviewRouter;
