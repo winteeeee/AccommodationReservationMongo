d2154a19227d4a980331605ee65aaabad521df85
wintee-desktop <b12e34e56@naver.com>
2023-12-07 22:19:38 +0900

    [server :: reservationController use]

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const {generateDummyData} = require("./faker");
 const app = express()
 const mongoose = require("mongoose").default
 const reviewController = require("./routers/reviewController");
+const reservationController = require("./routers/reservationController")
 /*
 참고) WebStorm에서 mongoose 관련 메소드들 자동완성을 사용하려면 require 뒤에 default 붙이거나 typescript 사용해야함
 mongoose가 typescript로 만들어져 있기 때문에 default 없이 require로 들고오면 메소드들을 못찾음
@@ -16,7 +17,8 @@ const server = async () => {
         await mongoose.connect(DB_URI)
         //await generateDummyData()
         app.use(express.json())
-        app.use("/review",reviewController);
+        app.use("/review", reviewController);
+        app.use("/reservation", reservationController);
         app.listen(port, hostname, function () {
             console.log(`서버 http://${hostname}:${port}/ 에서 실행 중`)
         })
