886d08ffa37820910589abe99fedf3e0cacce722
kitKimBeomSeok <126782965+kitKimBeomSeok@users.noreply.github.com>
2023-12-08 02:28:42 +0900

    숙소 조회 구현 (정렬 구현해야함) 페이커 별점 범위 1~5로 조정 client 숙소조회 추가 accommodationController.js get 추가

--- client.js
+++ client.js
@@ -5,10 +5,14 @@ console.log("클라이언트 실행 중")
 const client = async () => {
     console.log("[검사항목 1]")
     console.log("- 체크인, 체크아웃 날짜, 신청 인원, 숙소 종류를 입력하여 숙소조회를 진행한다")
-    const checkIn = rl.question("체크인 날짜 : ")
-    const checkOut = rl.question("체크아웃 날짜 : ")
-    const applicant = rl.question("신청 인원 : ")
-    const choice = rl.question('숙소 종류를 선택하세요 (1. 개인, 2. 공간 전체): ');
+    // const checkIn = rl.question("체크인 날짜 : ")
+    // const checkOut = rl.question("체크아웃 날짜 : ")
+    // const applicant = rl.question("신청 인원 : ")
+    // const choice = rl.question('숙소 종류를 선택하세요 (1. 개인, 2. 공간 전체): ');
+    const checkIn = '2023-12-01'
+    const checkOut = '2023-12-03'
+    const applicant = 1
+    const choice = '1'
     let houseType;
     if (choice === '1') {
         houseType = 'PRIVATE_ROOM';
@@ -21,25 +25,25 @@ const client = async () => {
     }
 
     await findHouse(checkIn, checkOut, applicant, houseType)
-    wait()
-
-    console.log("[검사항목 3]")
-    console.log("- 체크인, 체크아웃 날짜와 인원을 입력하여 예약을 진행한다")
-    const guestId = rl.question("게스트 ID : ")
-    const houseId = rl.question("숙소 ID : ")
-    const checkInDate = rl.question("체크인 날짜 : ")
-    const checkOutDate = rl.question("체크아웃 날짜 : ")
-    const person = rl.question("예약 인원 : ")
-    await bookHouse(guestId, houseId, checkInDate, checkOutDate, person)
-    wait()
-
-    console.log("[검사항목 6]")
-    console.log("- 게스트는 체크아웃이 완료된 숙소에 별점(1~5)와 후기를 작성할 수 있다")
-    const reserveId = rl.question("예약 ID : ")
-    const startRating = rl.question("별점 : ")
-    const reviewContent = rl.question("후기 : ")//
-    await submitReview(reserveId, startRating, reviewContent)
-    wait()
+    // wait()
+    //
+    // console.log("[검사항목 3]")
+    // console.log("- 체크인, 체크아웃 날짜와 인원을 입력하여 예약을 진행한다")
+    // const guestId = rl.question("게스트 ID : ")
+    // const houseId = rl.question("숙소 ID : ")
+    // const checkInDate = rl.question("체크인 날짜 : ")
+    // const checkOutDate = rl.question("체크아웃 날짜 : ")
+    // const person = rl.question("예약 인원 : ")
+    // await bookHouse(guestId, houseId, checkInDate, checkOutDate, person)
+    // wait()
+    //
+    // console.log("[검사항목 6]")
+    // console.log("- 게스트는 체크아웃이 완료된 숙소에 별점(1~5)와 후기를 작성할 수 있다")
+    // const reserveId = rl.question("예약 ID : ")
+    // const startRating = rl.question("별점 : ")
+    // const reviewContent = rl.question("후기 : ")//
+    // await submitReview(reserveId, startRating, reviewContent)
+    // wait()
 }
 client().then()
 
@@ -90,9 +94,21 @@ async function findHouse(checkinDate, checkoutDate, accommodatedPerson, houseTyp
                 houseType: houseType  // 여기에 houseType 추가
             }
         });
+        const accommodations = response.data.sortedAccommodations;
+
+
+        // 각 숙소의 이름, 가격, 별점만 추출하여 새로운 배열로 만듦
+        const result = accommodations.map(accommodation => {
+            return {
+                name: accommodation.name,
+                price: accommodation.totalPrice,
+                avgRating: accommodation.avgRating,
+            };
+        });
+
 
         // 여기서 response를 처리하거나 반환할 수 있습니다.
-        console.log('숙소 검색 결과:', response.data);
+        console.log('숙소 검색 결과:', result);
     } catch (error) {
         console.error('숙소 검색 중에 오류 발생:', error.response ? error.response.data : error.message);
     }
